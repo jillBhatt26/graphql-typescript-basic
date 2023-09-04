617efc96e807faba3472c924282008e0e7a73408
jill bhatt <bhattjill@gmail.com>
2023-09-04 19:25:30 +0530

    Added games mutations

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,3 +1,4 @@
+import { nanoid } from 'nanoid';
 import db from '../db';
 import { TAuthorArgs, TGameArgs, TReviewArgs } from '../types';
 
@@ -26,6 +27,72 @@ const resolvers = {
             const authorID = args.id;
 
             return db.authors.find(author => author.id === authorID);
+        }
+    },
+    Game: {
+        reviews(parent: any) {
+            return db.reviews.filter(review => review.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent: any) {
+            return db.reviews.filter(review => review.author_id === parent.id);
+        }
+    },
+    Review: {
+        author(parent: any) {
+            return db.authors.find(author => author.id === parent.author_id);
+        },
+        game(parent: any) {
+            return db.games.find(game => game.id === parent.game_id);
+        }
+    },
+    Mutation: {
+        addGame(
+            _: any,
+            args: { newGameData: { title: string; platform: string[] } }
+        ) {
+            const newGame = { ...args.newGameData, id: nanoid() };
+
+            db.games.push(newGame);
+
+            return newGame;
+        },
+        deleteGame(_: any, args: { gameID: string }) {
+            db.games = db.games.filter(game => game.id !== args.gameID);
+
+            return db.games.find(game => game.id === args.gameID) === undefined;
+        },
+        updateGame(
+            _: any,
+            args: {
+                editGameData: {
+                    id: string;
+                    edits: {
+                        title?: string;
+                        platform?: string[];
+                    };
+                };
+            }
+        ) {
+            const {
+                editGameData: { id, edits }
+            } = args;
+
+            const toUpdateGameIndex = db.games.findIndex(
+                game => game.id === id
+            );
+
+            if (toUpdateGameIndex < 0) return;
+
+            const updatedGameData = {
+                ...db.games[toUpdateGameIndex],
+                ...edits
+            };
+
+            db.games[toUpdateGameIndex] = updatedGameData;
+
+            return updatedGameData;
         }
     }
 };
--- src/schema/index.ts
+++ src/schema/index.ts
@@ -9,18 +9,22 @@ const typeDefs = `#graphql
         id: ID!
         title: String!
         platform: [String!]!
+        reviews: [Review!]
     }
 
     type Review {
         id: ID!
         rating: Int!
         content: String!
+        game: Game!
+        author: Author!
     }
 
     type Author {
         id: ID!
         name: String!
         verified: Boolean!
+        reviews: [Review!]
     }
 
     # NOTE: The query type is mandatory in every graphql typeDef string
@@ -31,6 +35,22 @@ const typeDefs = `#graphql
         game(id: ID!): Game
         authors: [Author]
         author(id: ID!): Author
+    }
+
+    type Mutation {
+        addGame(newGameData: AddGameInput!): Game
+        deleteGame(id: ID!): Boolean
+        updateGame(id: ID!, EditGameInput: EditGameInput!): Game
+    }
+
+    input AddGameInput {
+        title: String!
+        platform: [String!]!
+    }
+
+    input EditGameInput {
+        title: String
+        platform: [String!]
     }
 `;
 
